21d04b997af57facc382d123cbdccc373b4a15e5
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-06-24 02:32:23 +0900

    feat:(#84) useInput 커스텀훅 구현

---
+++ frontend/src/hooks/useInput.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function useInput(defaultValue: string = '') {
+  const [value, setValue] = useState(defaultValue);
+  const onChange = ({ target }: { target: HTMLInputElement }) => {
+    setValue(target.value);
+  };
+  return { value, onChange };
+}
