c332ff6e25b9b5c0b84c88015ef73f675561a7f6
junamee <skawn11114@gmail.com>
2021-06-24 18:00:14 +0900

    (feat) #82. 마일스톤 편집기능을 위한 뷰/편집 컴포넌트 분리

--- frontend/src/page/milestonePage/MilestoneAdd.tsx
+++ frontend/src/page/milestonePage/MilestoneAdd.tsx
@@ -1,19 +1,52 @@
+import React, { useState, ReactElement, ChangeEvent, Dispatch, SetStateAction } from 'react'
 import styled from 'styled-components'
 import Title from 'components/atom/Title'
 import PrimaryButton from 'components/atom/PrimaryButton'
-export default function MilestoneAdd(){
+import { fetchCreateMilestone } from 'util/api/fetchHandleMilestone'
+import PrimaryOutlinedButton from'components/atom/PrimaryOutlinedButton'
+import { MilestoneType } from 'components/common/tabModal/tapDataType'
+interface EditType{
+  type?: string
+  milestone?: MilestoneType
+  setEditMode?: Dispatch<SetStateAction<boolean>>
+}
 
+
+export default function MilestoneAdd({type='create', setEditMode, milestone}:EditType){
+  const initTitle = (!!milestone)?milestone.title:'마일스톤 제목'
+  const initDate = (!!milestone)?milestone.dueDate:'ex.YYYY-MM-DD'
+  const initDesc = (!!milestone)?milestone.description:'설명'
+  const [milestoneInputs, setMilestoneInputs] = useState({title:initTitle, dueDate:initDate, description:initDesc})
+  const {title, dueDate, description} = milestoneInputs
+ 
+  const handleSubmit = () => {
+    const newMilestone = {title, dueDate, description}
+    if(type==='create') fetchCreateMilestone(newMilestone)
+    // else {수정요청}
+    if(setEditMode) setEditMode(false)
+  }
+
+  const handleChange = (type: string, e:ChangeEvent<HTMLInputElement>) => {
+    if(type==='title') setMilestoneInputs({...milestoneInputs, title: e.target.value})
+    if(type==='dueDate') setMilestoneInputs({...milestoneInputs, dueDate: e.target.value})
+    if(type==='description') setMilestoneInputs({...milestoneInputs, description: e.target.value})
+  }
+
+  const handleClick = () => {}
+
+  const pageTitle = (type==='create')?'새로운 마일스톤 추가':'마일스톤 편집'
   return (
     <MilestoneAddBlock>
-      <Title className='milestone__add'>새로운 마일스톤 추가</Title>
-      <div className='milestone__add__input'>
+      <Title className='milestone__add'>{pageTitle}</Title>
+      <MilestoneInputBlock>
         <div>
-          <input type='text' className='milestone__name' placeholder='마일스톤 이름'/>
-          <input type='text' className='milestone__due-date' placeholder='완료일(선택) ex.YYYY-MM-DD'/>
+          <input type='text' className='milestone__name' value={title} onChange={(e)=>handleChange('title', e)}/>
+          <input type='text' className='milestone__due-date' placeholder='완료일(선택) ex.YYYY-MM-DD' value={dueDate} onChange={(e)=>handleChange('dueDate', e)}/>
         </div>
-        <input type='text' className='milestone__due-date' placeholder='설명(선택)'/>
-      </div>
-      <div className='milestone__add__submit'>
+        <input type='text' className='milestone__description' placeholder='설명(선택)' value={description} onChange={(e)=>handleChange('description', e)}/>
+      </MilestoneInputBlock>
+      <div className='milestone__add__submit' onClick={handleSubmit}>
+        {type!=='create' && <PrimaryOutlinedButton value={'× 취소'} onClick={handleClick}/>}
         <PrimaryButton value={'+ 완료'}/>
       </div>
     </MilestoneAddBlock>
@@ -25,34 +58,34 @@ margin: 24px 0;
 padding: 32px;
 border-radius: 16px;
 border: 1px solid ${({ theme }) => theme.color.lineGrey};
-.milestone__add__input{
-  div{
-  display: flex;
-  margin-top: 32px ;
-  }
-  input{
-    display: block;
-    width: 50%;
-    height: 56px;
-    padding: 0 24px;
-    margin-right: 16px;
-    margin-bottom: 16px;
-    border: none;
-    background-color: ${({ theme }) => theme.color.inputBg};
-    font-size: ${({ theme }) => theme.size.md}px;
-    border-radius: 11px;
-    &:focus {
-    text-decoration: none;
-    background-color: ${({ theme }) => theme.color.white};
-    }
-    .milestone__due-date{
-      margin: 0 0 24px 0;
-      width: 100%;
-    }
-  }
-}
 .milestone__add__submit{
   display: flex;
   justify-content: flex-end;
 }
+`
+
+const MilestoneInputBlock = styled.div`
+div{
+  display: flex;
+  margin-top: 32px;
+}
+input{
+  display: block;
+  width: 50%;
+  height: 56px;
+  padding: 0 24px;
+  margin-right: 16px;
+  margin-bottom: 16px;
+  border: none;
+  background-color: ${({ theme }) => theme.color.inputBg};
+  font-size: ${({ theme }) => theme.size.md}px;
+  border-radius: 11px;
+  &:focus {
+  text-decoration: none;
+  background-color: ${({ theme }) => theme.color.white};
+  }
+  .milestone__due-date{
+    margin: 0 0 24px 0;
+    width: 100%;
+  }
 }`
--- frontend/src/page/milestonePage/MilestoneItem.tsx
+++ frontend/src/page/milestonePage/MilestoneItem.tsx
@@ -1,93 +1,12 @@
-import styled from 'styled-components'
-import MilestoneIcon from 'components/atom/MilestoneIcon'
-import { ReactComponent as DeleteIcon} from 'assets/icon/DeleteIcon.svg';
-import { ReactComponent as EditIcon} from 'assets/icon/EditIcon.svg';
-import { ReactComponent as CloseIcon} from 'assets/icon/CloseIcon.svg';
-import { ReactComponent as CalendarIcon} from 'assets/icon/CalendarIcon.svg';
-import CustomizedProgressBars from 'components/atom/Progress'
+import React, { useState } from 'react'
 import { MilestoneType } from 'components/common/tabModal/tapDataType'
-import fetchHandleMilestone from 'util/api/fetchHandleMilestone'
+import MilestoneAdd from'page/milestonePage/MilestoneAdd'
+import MilestoneInfo from 'page/milestonePage/MilestoneInfo'
 interface MilestoneItemType{
   milestone: MilestoneType
 }
 export default function MilestoneItem({milestone}:MilestoneItemType){
-  
-  const {id, title, description, dueDate, openedIssueCount, closedIssueCount}:MilestoneType = milestone
-  const handleClose = () => {
-    fetchHandleMilestone(id)
-  }
-    return (
-    <MilestoneItemBlock>
-      <div className='milestone__list__left'>
-        <Title>
-          <div><MilestoneIcon/>&nbsp;{title}</div>
-          <div><CalendarIcon/>&nbsp;{dueDate}</div>
-        </Title>
-        <div className='milestone__list__desc'>&nbsp;&nbsp;{description}</div>
-      </div>
-      <div className='milestone__list__right'>
-        <div className='milestone__list__edit'>
-          <div onClick={handleClose} className='closeBtn'><CloseIcon/><div>닫기</div></div>
-          <div><EditIcon/><div>편집</div></div>
-          <div><DeleteIcon/><div className='delete'>삭제</div></div>
-        </div>
-        <div className='milestone__list__progress'>
-          <CustomizedProgressBars progress={50}/>
-          <div className='milestone__state'>
-            <div>50%</div>
-            <div>열린 이슈({openedIssueCount})  &nbsp;&nbsp;닫힌 이슈({closedIssueCount})</div>
-          </div>
-        </div>
-      </div>
-    </MilestoneItemBlock>
-  )}
-
-const Title = styled.div`
-display: flex;
-div{ 
-  display: flex;
-  align-items:center;
+  const [isEditMode, setEditMode] = useState(false)
+  return !isEditMode?<MilestoneInfo setEditMode={setEditMode} milestone={milestone}/>:<MilestoneAdd type='edit' setEditMode={setEditMode} milestone={milestone}/>
 }
-div:first-child{
-  font-size: ${({ theme }) => theme.size.md2}px;
-  font-weight: 800;
-  margin-right: 10px;
-}
-}`
-
-const MilestoneItemBlock = styled.div`
-display: flex;
-justify-content: space-between;
-border-top: 1px solid ${({ theme }) => theme.color.lineGrey};
-padding: 10px;
-.milestone__list__desc{
-  display: flex;
-  padding-top: 5px;
-}
-.milestone__list__right{
-  width: 245px;
-}
-.milestone__list__edit{
-  cursor: pointer;
-  display: flex; 
-  font-size: ${({ theme }) => theme.size.sm}px;
-  justify-content: flex-end;
-  div{
-    display: flex;
-    align-items: center;
-    margin-left: 20px;
-    div{
-      margin-left: 5px;
-    }
-  }
-}
-.milestone__state{
-  display: flex;
-  justify-content: space-between;
-  font-size: ${({ theme }) => theme.size.sm}px;
-}
-.delete{
-  color: ${({theme})=>theme.color.red};
-}
-`
 
