9156970b347f0b9a35c447172d5dd8f3af4130df
junamee <skawn11114@gmail.com>
2021-06-29 11:53:55 +0900

    Refactor: (#57) 변경한api path로 적용

--- frontend/src/store/labelMilestoneStore.ts
+++ frontend/src/store/labelMilestoneStore.ts
@@ -2,47 +2,47 @@ import { atom, selector, selectorFamily } from 'recoil';
 import API, { authorizedHeaders } from 'util/api/api';
 //레이블,모달 클릭 감지 및 리셑_____________________________________
 interface labelMilestoneStateType {
-  label: boolean;
-  milestone: boolean;
+	label: boolean;
+	milestone: boolean;
 }
 
 export const labelMilestoneClickedState = atom<labelMilestoneStateType>({
-  key: 'labelMilestoneState',
-  default: { label: false, milestone: false },
+	key: 'labelMilestoneState',
+	default: { label: false, milestone: false },
 });
 
 export const resetTabClickedState = selector({
-  key: 'resetTabClickedState',
-  get: ({ get }) => {
-    const tabClickedState = get(labelMilestoneClickedState);
-    return tabClickedState;
-  },
-  set: ({ reset }) => {
-    reset(labelMilestoneClickedState);
-  },
+	key: 'resetTabClickedState',
+	get: ({ get }) => {
+		const tabClickedState = get(labelMilestoneClickedState);
+		return tabClickedState;
+	},
+	set: ({ reset }) => {
+		reset(labelMilestoneClickedState);
+	},
 });
 
 //마일즈스톤___________________________________________
 
 export const milestoneTrigger = atom<number>({
-  key: 'milestoneTrigger',
-  default: 0,
+	key: 'milestoneTrigger',
+	default: 0,
 });
 export const getMilestones = selectorFamily({
-  key: 'GET/milestones',
-  get:
-    (status: string) =>
-    async ({ get }) => {
-      const token = localStorage.getItem('token');
-      try {
-        get(milestoneTrigger);
-        const response = await fetch(API.getMilestone(status), {
-          headers: authorizedHeaders(token),
-        });
-        const milestoneData = await response.json();
-        return milestoneData;
-      } catch (error) {
-        console.log('마일스톤조회 에러:', error);
-      }
-    },
+	key: 'GET/milestones',
+	get:
+		(status: string) =>
+		async ({ get }) => {
+			const token = localStorage.getItem('token');
+			try {
+				get(milestoneTrigger);
+				const response = await fetch(API.MILESTONE.GET(status), {
+					headers: authorizedHeaders(token),
+				});
+				const milestoneData = await response.json();
+				return milestoneData;
+			} catch (error) {
+				console.log('마일스톤조회 에러:', error);
+			}
+		},
 });
