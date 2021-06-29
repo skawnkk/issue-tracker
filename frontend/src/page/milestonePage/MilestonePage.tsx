42bfebe0e5830064d52a3f3b512c533aa3c7a19c
junamee <skawn11114@gmail.com>
2021-06-29 11:05:56 +0900

    refactor: (#82) 마일스톤 컴포넌트 분리(마일스톤페이지->테이블/리스트)

--- frontend/src/page/milestonePage/MilestonePage.tsx
+++ frontend/src/page/milestonePage/MilestonePage.tsx
@@ -1,81 +1,40 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { labelMilestoneClickedState, getMilestones } from 'store/labelMilestoneStore';
-import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { labelMilestoneClickedState } from 'store/labelMilestoneStore';
+import { useSetRecoilState } from 'recoil';
 import LabelMilestoneTab from 'components/common/LabelMilestoneTab';
 import PrimaryButton from 'components/atom/PrimaryButton';
 import PrimaryOutlinedButton from 'components/atom/PrimaryOutlinedButton';
-import MilestoneIcon from 'components/atom/MilestoneIcon';
-import MilestoneAdd from 'page/milestonePage/MilestoneAdd';
-import MilestoneItem from 'page/milestonePage/MilestoneItem';
-import { ReactComponent as CloseIcon } from 'assets/icon/CloseIcon.svg';
-import { MilestoneType } from 'components/common/tabModal/tapDataType'
+import MilestoneEditForm from 'page/milestonePage/MilestoneEditForm';
+import MilestoneTable from 'page/milestonePage/milestoneTable/MilestoneTable';
 export default function MilestonePage() {
-  const status = 'open'
-  const {closedMilestonesCount, openedMilestonesCount, milestones} = useRecoilValue(getMilestones(status));
+	const setLabelMilestoneState = useSetRecoilState(labelMilestoneClickedState);
+	setLabelMilestoneState({ label: false, milestone: true });
 
-  const setLabelMilestoneState = useSetRecoilState(labelMilestoneClickedState);
-  setLabelMilestoneState({ label: false, milestone: true });
+	const [addClick, setAddClick] = useState(false);
+	const handleClick = () => setAddClick(!addClick);
 
-  const [addClick, setAddClick] = useState(false);
-  const handleClick = () => setAddClick(!addClick);
-  
-
-  const milestoneList = milestones.map((milestone:MilestoneType)=><MilestoneItem key={milestone.id} milestone={milestone}/>)
-  
-  return (
-    <MilestoneBlock>
-      <div className='tab__option__header'>
-        <LabelMilestoneTab />
-        {!addClick 
-          ? (<PrimaryButton value={'+ 추가'} onClick={handleClick} />)
-          : (<PrimaryOutlinedButton value={'× 닫기'} onClick={handleClick} />)}
-      </div>
-      {addClick && <MilestoneAdd setEditMode={setAddClick}/>}
-      <div className='tab__table'>
-        <div className='tab__table__header'>
-          <div>
-            <div>
-              <MilestoneIcon />
-              열린 마일스톤({openedMilestonesCount})
-            </div>
-            <div>
-              <CloseIcon />
-              &nbsp;&nbsp;닫힌 마일스톤({closedMilestonesCount})
-            </div>
-          </div>
-        </div>
-        {milestoneList}
-      </div>
-    </MilestoneBlock>
-  );
+	return (
+		<MilestoneBlock>
+			<div className='tab__option__header'>
+				<LabelMilestoneTab />
+				{!addClick ? (
+					<PrimaryButton value={'+ 추가'} onClick={handleClick} />
+				) : (
+					<PrimaryOutlinedButton value={'× 닫기'} onClick={handleClick} />
+				)}
+			</div>
+			{addClick && <MilestoneEditForm setEditMode={setAddClick} />}
+			<MilestoneTable />
+		</MilestoneBlock>
+	);
 }
 
 const MilestoneBlock = styled.div`
-  padding: 50px 80px;
-  .tab__option__header {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 50px;
-  }
-  .tab__table {
-    border-radius: 16px;
-    border: 1px solid ${({ theme }) => theme.color.lineGrey};
-  }
-  .tab__table__header {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    background-color: ${({ theme }) => theme.color.bgGrey};
-    border-radius: 16px 16px 0 0;
-    div {
-      display: flex;
-      width: 300px;
-      div {
-        display: flex;
-        align-items: center;
-      }
-    }
-  }
+	padding: 50px 80px;
+	.tab__option__header {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 50px;
+	}
 `;
