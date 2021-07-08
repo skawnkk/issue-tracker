b77cded93bdcb48007ca9332248d009f13b3af31
junamee <skawn11114@gmail.com>
2021-07-08 13:33:52 +0900

    Feat. 마크다운 이미지파싱

---
+++ frontend/src/util/ImgParser.ts
@@ -0,0 +1,11 @@
+export default function ImgParser(comment: string) {
+  const matchedLink = /\!\[([^\]]+)\]\(([^\)]+)\)/g;
+  let stringComment = comment.replace(matchedLink, '');
+  debugger;
+  let imgURL1 = comment.match(matchedLink) as Array<string>;
+
+  if (!imgURL1) return null;
+
+  let imgURL = imgURL1[0].replace(matchedLink, `$2 $1`).split(' ');
+  return { comment: stringComment, imgLink: imgURL[0], imgDescription: imgURL[1] };
+}
