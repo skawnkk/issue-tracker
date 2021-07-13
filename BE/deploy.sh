20233367a7489987d2d86c137179570e22329e40
janeljs <jisunlim818@gmail.com>
2021-07-13 11:21:54 +0900

    chore: 배포 스크립트 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -5,5 +5,5 @@ JAR_NAME=$(ls $REPOSITORY/build/libs | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 fuser -k 8080/tcp
 #nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-nohup java -jar $JAR_PATH >> /home/ubuntu/deploy.log 2>&1 &
+nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=prod $JAR_PATH >> /home/ubuntu/deploy.log 2>&1 &
 
